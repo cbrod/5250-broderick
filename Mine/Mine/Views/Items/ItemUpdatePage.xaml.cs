64ecb45e5f7ee2b4f445ddc33657d0742566f651
cbrod <broderickcel@seattleu.edu>
2020-01-26 20:49:28 -0800

    Changed ItemUpdate to be a Modal Page

--- Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
@@ -36,8 +36,8 @@ namespace Mine.Views
         async void Save_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "Update", ViewModel.Data);
-            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
-            await Navigation.PopAsync();
+
+            await Navigation.PopModalAsync();
         }
 
         /// <summary>
@@ -47,15 +47,22 @@ namespace Mine.Views
         /// <param name="e"></param>
         async void Cancel_Clicked(object sender, EventArgs e)
         {
-            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
-
-            await Navigation.PopAsync();
+            await Navigation.PopModalAsync();
         }
 
         // Updates Value text to reflect update when stepper is triggered
         void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
             ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        /// <summary>
+        /// Disable Android back button on this page
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
         }
     }
 }
