41dddde585a14c6fa038a1f6505a59f84c199eeb
cbrod <broderickcel@seattleu.edu>
2020-01-26 19:43:22 -0800

    Added Update Method to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -9,5 +9,22 @@
 
         // the Value of the item
         public int Value { get; set; } = 0;
+
+        /// <summary>
+        /// Update the ItemModel
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(ItemModel data)
+        {
+            // Update Base
+            Name = data.Name;
+            Description = data.Description;
+
+            // Update the extended
+            Value = data.Value;
+
+            return true;
+        }
     }
 }
