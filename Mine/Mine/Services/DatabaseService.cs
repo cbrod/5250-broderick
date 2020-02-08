e542ad9f90363be296726c3a41895a82f8784567
cbrod <broderickcel@seattleu.edu>
2020-02-07 21:02:41 -0800

    Updated ReadAsync in DatabaseServices

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -40,9 +40,9 @@ namespace Mine.Services
             return await Database.InsertAsync(item);
         }
 
-        public Task<ItemModel> ReadAsync(string id)
+        public async Task<ItemModel> ReadAsync(string id)
         {
-            return Database.Table<ItemModel>().Where(i => i.Id.Equals(id)).FirstOrDefaultAsync();
+            return await Database.Table<ItemModel>().Where(i => i.Id.Equals(id)).FirstOrDefaultAsync();
         }
 
         public async Task<bool> UpdateAsync(ItemModel item)
